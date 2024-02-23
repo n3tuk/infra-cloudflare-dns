7d44ca54d2c220b9ad7011e194ba5730ba4617ec
Jonathan Wright <jon@than.io>
2024-02-23 19:16:49 +0000

    Correct the IPv6 address for testing-01
    Correct the IPv6 address for the testing-01 host in the testing
    environment following a change to the MAC address on the network
    interface.

--- terraform/kub3-testing.tf
+++ terraform/kub3-testing.tf
@@ -4,7 +4,7 @@ locals {
       environment = "testing"
       region      = "cym-south-1"
       ipv4        = "172.23.39.2"
-      ipv6        = "2a02:8010:8006:3a39:c7:b6ff:fe06:98c3"
+      ipv6        = "2a02:8010:8006:3a39:65:a6ff:fee8:a87a"
     },
     { name        = "testing-02"
       environment = "testing"
