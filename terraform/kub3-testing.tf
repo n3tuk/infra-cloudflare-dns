e51ba48d21cc9b315be5e7401e0bebccb86d320b
Jonathan Wright <jon@than.io>
2024-02-23 17:51:16 +0000

    Add testing-02 Kubernetes node for Testing

--- terraform/kub3-testing.tf
+++ terraform/kub3-testing.tf
@@ -6,5 +6,11 @@ locals {
       ipv4        = "172.23.39.2"
       ipv6        = "2a02:8010:8006:3a39:c7:b6ff:fe06:98c3"
     },
+    { name        = "testing-02"
+      environment = "testing"
+      region      = "cym-south-1"
+      ipv4        = "172.23.39.3"
+      ipv6        = "2a02:8010:8006:3a39:bb:79ff:fe09:18a7"
+    },
   ]
 }
